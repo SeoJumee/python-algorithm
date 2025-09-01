84d81d2b95c3b541b6eeed47970ebcf4b567748a
SeoJumee <official.jumee@gmail.com>
2025-09-01 19:42:52 +0900

    [swea] 12712 - 파리퇴치3

--- swea/12712/파리퇴치3.py
+++ swea/12712/파리퇴치3.py
@@ -13,21 +13,28 @@ for tc in range(1, T + 1):
     for i in range(N):
         for j in range(N):
 
+            # 현재 값
             value = matrix[i][j]
+
+            # + 와 x 모양 합계를 각각 계산
             delta1_sum_value = value
             delta2_sum_value = value
 
+            # 분사 세기(M) 맞춰 중심으로부터 k만큼 떨어진 위치
             for k in range(1, M):
+                # + 모양
                 for di, dj in delta1:
                     ni, nj = i + k * di, j + k * dj
                     if 0 <= ni < N and 0 <= nj < N:
                         delta1_sum_value += matrix[ni][nj]
 
+                # x 모양
                 for di, dj in delta2:
                     ni, nj = i + k * di, j + k * dj
                     if 0 <= ni < N and 0 <= nj < N:
                         delta2_sum_value += matrix[ni][nj]
 
-                max_value = max(max_value, delta1_sum_value, delta2_sum_value)
+            # 전체 최댓값과 비교해 갱신
+            max_value = max(max_value, delta1_sum_value, delta2_sum_value)
 
     print(f"#{tc} {max_value}")
